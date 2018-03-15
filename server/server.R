bf148a06bc44fa5c373545e054d6ab7e737827f0
Atichat43 <boss_atln@outlook.com>
2018-03-15 14:12:55 +0700

    first draft ui homeTeam information

--- server/server.R
+++ server/server.R
@@ -1,3 +1,143 @@
-output$playerslist<-renderUI({
-  selectInput("playerslist", "Select Data", choices=getData(), selected=playerslist[1])
-})
+library(shinydashboard)
+server <- function(input, output) {
+  
+  output$PlaySpeed<- renderText({
+    paste("PlaySpeed",input$PlaySpeed)
+  })
+  
+  output$PlayDribbing <- renderText({
+    paste("PlayDribbing",input$PlayDribbing)
+  })
+  
+  output$PlayPassing <- renderText({
+    paste("PlayPassing",input$PlayPassing)
+  })
+  
+  output$PlayPositioning <- renderText({
+    paste("PlayPositioning",input$PlayPositioning)
+  })
+  
+  output$ChanceCreationPassing<- renderText({
+    paste("ChanceCreationPassing",input$ChanceCreationPassing)
+  })
+  
+  output$ChanceCreationCross<- renderText({
+    paste("ChanceCreationCross",input$ChanceCreationCross)
+  })
+  
+  output$ChanceCreationShooting<- renderText({
+    paste("ChanceCreationShooting",input$ChanceCreationShooting)
+  })
+  
+  output$DefencePressure<- renderText({
+    paste("DefencePressure",input$DefencePressure)
+  })
+  
+  output$DefenceAggretion<- renderText({
+    paste("DefenceAggretion",input$DefenceAggretion)
+  })
+  
+  output$DefenceTeamWidth<- renderText({
+    paste("DefenceTeamWidth",input$DefenceTeamWidth)
+    
+  })
+  
+  vals <- reactiveValues(data = NULL)
+  dataModal <- function(failed = FALSE) {
+    modalDialog(
+      selectInput("dataset", "Select Data",getData(""),selected="Name 1" ,multiple = FALSE),
+      span('(select one player)'),
+      
+      footer = tagList(
+        modalButton("Cancel"),
+        actionButton("ok", "OK")
+      )
+    )
+  }
+  
+  lapply(1:10,function(i){
+    observeEvent(input[[paste0("show",i)]], {
+      showModal(dataModal())
+    })
+  })
+  
+  observeEvent(input$ok, {
+    vals$data <- ({input$dataset})
+    removeModal()
+    
+  })
+  
+  # Display information about selected data
+  output$dataInfo <- renderPrint({
+    if (is.null(vals$data))
+      "No data selected"
+    else
+      vals$data
+  })
+  
+  
+  
+  output$ui <- renderUI({
+    if (is.null(input$Formation))
+      return()
+    switch(input$Formation,
+           "4-3-3" =   tagList (
+             lapply(1:10,function(i){
+               column(3,box(title="player",actionButton(paste0("show",i),paste0("player",i))))                   
+             })
+           ),
+           
+           
+           "4-4-2" = tagList (
+             column(3,
+                    box(title = "player1",actionButton("action", "player1"))),
+             column(3,
+                    box(title = "player2",actionButton("action", "player2"))),
+             column(3,
+                    box(title = "player3",actionButton("action", "player3"))),
+             column(3,
+                    box(title = "player4",actionButton("action", "player4"))),
+             column(3,
+                    box(title = "player5",actionButton("action", "player5"))),
+             column(3,
+                    box(title = "player6",actionButton("action", "player6"))),
+             column(3,
+                    box(title = "player7",actionButton("action", "player7"))),
+             column(3,
+                    box(title = "player8",actionButton("action", "player8"))),
+             column(3,
+                    box(title = "player9",actionButton("action", "player9"))),
+             column(3,
+                    box(title = "player10",actionButton("action", "player10"))),
+             column(3,
+                    box(title = "player11",actionButton("action", "player11")),offset = 5)
+           ),
+           "5-4-1" = tagList (
+             column(3,
+                    box(title = "player1",actionButton("action", "player1"))),
+             column(3,
+                    box(title = "player2",actionButton("action", "player2"))),
+             column(3,
+                    box(title = "player3",actionButton("action", "player3"))),
+             column(3,
+                    box(title = "player4",actionButton("action", "player4"))),
+             column(3,
+                    box(title = "player5",actionButton("action", "player5"))),
+             column(3,
+                    box(title = "player6",actionButton("action", "player6"))),
+             column(3,
+                    box(title = "player7",actionButton("action", "player7"))),
+             column(3,
+                    box(title = "player8",actionButton("action", "player8"))),
+             column(3,
+                    box(title = "player9",actionButton("action", "player9"))),
+             column(3,
+                    box(title = "player10",actionButton("action", "player10"))),
+             column(3,
+                    box(title = "player11",actionButton("action", "player11")))
+           )
+           
+    )
+  })
+}
+
--- ui/ui.R
+++ ui/ui.R
@@ -1,15 +1,80 @@
 # Define UI for app that draws a histogram ----
-fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      
-      selectInput("playerslist", "Select Data",getData(""),selected="Name 1" ,multiple = FALSE),
-      
-      
-      submitButton("Predict")),
+library(shinydashboard)
+
+header <- dashboardHeader(
+  title = "Derdy Match Simulator"
+)
+
+body <- dashboardBody(
+  # Boxes need to be put in a row (or column)
+  fluidRow(
+    column(width = 3,
+           box(width = NULL,title = "Tactic",
+               selectInput("PlaySpeed", label = h5("PlaySpeed"), 
+                           choices = list("Balance" = 1, "Fast" = 2, "Slow" = 3), 
+                           selected = 1),
+               
+               selectInput("PlayDribbling", label = h5("PlayDribbling"), 
+                           choices = list("Little" = 1, "Normal" = 2),
+                           selected = 1),
+               
+               selectInput("playPassing", label = h5("playPassing"), 
+                           choices = list("Long" = 1, "Mixed" = 2, "Short" = 3), 
+                           selected = 1),
+               
+               selectInput("PlayPositioning", label = h5("PlayPositioning"), 
+                           choices = list("Free Form" = 1, "Organised" = 2),
+                           selected = 1),
+               
+               selectInput("ChanceCreationPassing", label = h5("ChanceCreationPassing"), 
+                           choices = list("Normal" = 1, "Risky" = 2, "Safe" = 3), 
+                           selected = 1),
+               
+               selectInput("ChanceCreationCross", label = h5("ChanceCreationCross"), 
+                           choices = list("Little" = 1, "Lots" = 2, "Normal" = 3), 
+                           selected = 1),
+               
+               selectInput("ChanceCreationShooting", label = h5("ChanceCreationShooting"), 
+                           choices = list("Little" = 1, "Lots" = 2, "Normal" = 3), 
+                           selected = 1),
+               
+               selectInput("ChanceCreationPosition", label = h5("ChanceCreationPosition"), 
+                           choices = list("Free Form" = 1, "Organised" = 2), 
+                           selected = 1),
+               
+               selectInput("DefencePressure", label = h5("DefencePressure"), 
+                           choices = list("Deep" = 1, "High" = 2, "Medium" = 3), 
+                           selected = 1),
+               
+               selectInput("DefenceAggression", label = h5("DefenceAggression"), 
+                           choices = list("Contain" = 1, "Double" = 2, "Press" = 3), 
+                           selected = 1),
+               
+               selectInput("DefenceTeamWidth", label = h5("DefenceTeamWidth"), 
+                           choices = list("Narrow" = 1, "Normal" = 2, "Wide" = 3), 
+                           selected = 1),
+               
+               actionButton("ok", "OK"))
+    ),
     
-    mainPanel(
-      textOutput("PLAYER")
+    column(width = 9,
+           box(width = NULL, title = "Formular",wellPanel(
+             selectInput("Formation", "Formation",
+                         c("4-3-3","4-4-2","5-4-1")
+             )
+           )),
+           box(width = NULL, title = "Player",
+               uiOutput("ui")
+               
+           )
     )
   )
 )
+
+dashboardPage(
+  header,
+  dashboardSidebar(disable = TRUE),
+  body
+)
+
+
