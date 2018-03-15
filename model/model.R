5e19e91bc91d7d6b079cd5515612102f3a5dbdf6
Atichat43 <boss_atln@outlook.com>
2018-03-15 10:32:24 +0700

    change model to library(RSNNS), delegation function predictor and model

--- model/model.R
+++ model/model.R
@@ -1,4 +1,7 @@
 #Model
+
+#FOR RKEEL 
+"
 seperateDataset <- function(dataset, index_of_y=length(names(dataset)), percentage=0.75) {
   library(caret) 
   inTrain <- createDataPartition(y=dataset[,index_of_y], p=percentage, list=FALSE) 
@@ -7,19 +10,42 @@ seperateDataset <- function(dataset, index_of_y=length(names(dataset)), percenta
  list(Train <- training_set, Test <- testing_set)
 } #return value: list that [[1]] training_set, [[2]] testing_set
 
+
 trainModel <- function(train_set, test_set) {
   library(RKEEL)
   mlp_bp_c_model <- MLP_BP_C(as.data.frame(train_set), as.data.frame(test_set), hidden_layers = 1, hidden_nodes = 41)
   mlp_bp_c_model$run()
   return(mlp_bp_c_model)
 }
+"
+#FOR RSNNS with normlization
+seperateDataset <- function(datasets) {
+  library(RSNNS)
+  datasets <- datasets[sample(1:nrow(datasets),length(1:nrow(datasets))),1:ncol(datasets)]
+  
+  datasetsValues <- datasets[,1:length(datasets)-1]
+  datasetsTargets <- decodeClassLabels(datasets[,length(datasets)])
+  
+  datasets <- splitForTrainingAndTest(datasetsValues, datasetsTargets, ratio=0.15)
+  datasets <- normTrainingAndTestSet(datasets)
+  return(datasets)
+}
+
+preparingData <- function(filenames="dataset_numeric.csv") {
+  datasets <- read.csv(file.path("datasets", "dataset_numeric.csv"))
+  datasets <- seperateDataset(datasets)
+}
+
+datasets <- preparingData()
+
 
 Model <- function() {
-  datasets <- read.csv(file.path("datasets", "dataset.csv"))
-  datasets <- seperateDataset(datasets)
-  model <- trainModel(datasets[1], datasets[2])
+  model <- trainModel()
   return(model)
 }
 
-#pd <- mlp_bp_c_model$testPredictions
-#table(pd)
+trainModel <- function(){
+  model <- mlp(x = datasets$inputsTrain, y = datasets$targetsTrain, 
+               inputsTest=datasets$inputsTest, targetsTest=datasets$targetsTest)
+  return(model)
+}
--- model/predictor.R
+++ model/predictor.R
@@ -1,14 +1,32 @@
 #Predictor
 source(file.path("model", "model.R"))
-m <- Model()
+model <- Model()
 
 getResult <- function(model, testData){
   return("win")
 }
 
 #========== for dev ========================
+"
 getAccuracy <- function(model){
   t <- table(model$testPredictions)
   acc <- (t[1] + t[5] + t[9])/sum(t[1:9])
   return(acc)
 }
+"
+getSummary <- function() {
+  summary(model)
+}
+
+getAccuracy <- function(){
+  predictions <- predict(model,datasets$inputsTest)
+  t <- confusionMatrix(datasets$targetsTrain, fitted.values(model))
+  accuracy(t)
+  t2 <- confusionMatrix(datasets$targetsTest,predictions)
+  accuracy(t2)
+}
+
+accuracy <- function(table){
+  return((table[1] + table[5] + table[9])/sum(table[1:9]))
+}
+
