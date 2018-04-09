7ae2d0f4f2a2f54f7ecda63c1dadb6f43abf05fd
Atichat43 <boss_atln@outlook.com>
2018-04-09 21:14:28 +0700

    update player_attr every team for ty

--- dataconnector.R
+++ dataconnector.R
@@ -13,14 +13,14 @@ getDataset <- function(){
 
 getPlayers <- function(teamName){
   table_players <- read.csv(file.path('datasets', 'players', paste('Player_', teamName, '.csv', sep="")))
-  table_players <- subset(table_players, select = c(player_name, player_position, player_number))
+  #table_players <- subset(table_players, select = c(player_name, player_position, player_number))
   return(table_players)
 }
 
 toStringPlayers <- function(players){
   arr <- as.array(as.character(1:nrow(players)))
   for(nr in 1:nrow(players)){
-    arr[nr] <- paste(as.character(players[nr, 1]), as.character(players[nr, 2]), as.character(players[nr, 3]), sep=" / ")
+    arr[nr] <- paste(as.character(players$player_name[nr]), as.character(players$player_position[nr]), as.character(players$player_number[nr]), sep=" / ")
   }
   return(arr)
 }
