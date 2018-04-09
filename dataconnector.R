9995be1f36ea3eb1f5d7f9d076e6af5ce912f8a6
DESKTOP-1BJRLJT\Gift <s8090015@gmail.com>
2018-04-09 21:27:52 +0700

    get api player in server file

--- dataconnector.R
+++ dataconnector.R
@@ -13,14 +13,22 @@ getDataset <- function(){
 
 getPlayers <- function(teamName){
   table_players <- read.csv(file.path('datasets', 'players', paste('Player_', teamName, '.csv', sep="")))
-  table_players <- subset(table_players, select = c(player_name, player_position, player_number))
+  #table_players <- subset(table_players, select = c(player_name, player_position, player_number))
   return(table_players)
 }
 
 toStringPlayers <- function(players){
   arr <- as.array(as.character(1:nrow(players)))
   for(nr in 1:nrow(players)){
-    arr[nr] <- paste(as.character(players[nr, 1]), as.character(players[nr, 2]), as.character(players[nr, 3]), sep=" / ")
+    arr[nr] <- paste(as.character(players[nr, 3]), as.character(players[nr, 4]), as.character(players[nr, 5]), sep=" / ")
   }
   return(arr)
+}
+
+apiPlayers <- function(players){
+  api <- as.array(as.character(1:nrow(players)))
+  for(nr in 1:nrow(players)){
+    api[nr] <- as.character(players[nr, 2])
+  }
+  return(api)
 }
