ced3fadf3f5f2810d58c8cbcf53b779eadfb7161
mightymity <mity_dekaranger@hotmail.com>
2018-04-24 21:14:50 +0700

    Revert "hai boss tum ngarn"
    This reverts commit b567243cb1ff66a0346e7dafd44fbf0680678d98.

--- ui/tab4.R
+++ ui/tab4.R
@@ -1,9 +1,10 @@
 shiny::fluidRow(
   column(width = 4,
-         box(width = NULL,title = "MANCHESTER UNITED",
-             uiOutput(outputId = "HomeApi")
-             )
-  
+         box(width = NULL,title = "MANCHESTER UNITED"),
+         radioButtons("radio", label = h3("Choose one"),
+                      choices = list("Ashley Young" = 1, "Bastian Schweinsteger" = 2, "Wayne Rooney" = 3,"Michael Carrick" = 1, "Antonio Valencia" = 2, "Marouane Fellaini" = 3,
+                                     "Juan Mata" = 1, "Matteo Darmian" = 2, "Morgan Schneiderlin" = 3,"Sergio Romeo" = 2, "Marcos Rojo" = 3), 
+                      selected = 1)
   ),
   
   column(width = 4,
@@ -12,6 +13,9 @@ shiny::fluidRow(
   ),
   column(width = 4,
          box(width = NULL, title = "Away Tactic"),
-         textOutput("PlaySpeedA")
+         radioButtons("radio2", label = h3("Choose one"),
+                      choices = list("David Ospina " = 1, "Per Mertesacker" = 2, "Laurent Koscielny" = 3,"Nacho Monreal " = 1, "Hector Bellerin " = 2, "Mesut Oezil" = 3,
+                                     "Aaron Ramsey" = 1, "Francis Coquelin" = 2, "Olivier Giroud " = 3,"Alexis Sanchez " = 2, "Danny Welbeck" = 3), 
+                      selected = 1)
 )
 )
